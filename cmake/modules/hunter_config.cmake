8ab5658454709e688305779b79782ff701b3e26e
Юдинцев Роман <r.yudintsev@2gis.ru>
2017-10-30 17:27:32 +0700

    36

--- cmake/modules/hunter_config.cmake
+++ cmake/modules/hunter_config.cmake
@@ -78,7 +78,7 @@ macro(hunter_config)
   endif()
 
   string(COMPARE NOTEQUAL "${_hunter_LOCAL_DIR}" "" _hunter_localdir_consume)
-  message(STATUS _hunter_localdir_consume)
+  message(STATUS ${_hunter_localdir_consume})
   if(_hunter_localdir_consume)
     set(HUNTER_${_hunter_current_project}_LOCAL_DIR "${_hunter_LOCAL_DIR}")
     set(_hunter_VERSION, "FROM_LOCAL_PATH")
